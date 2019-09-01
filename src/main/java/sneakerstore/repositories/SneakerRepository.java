c94e3b445462f02c129822fd19f3e20b35c6bb14
jeremybreccion <34333326+jeremybreccion@users.noreply.github.com>
2019-09-01 13:04:52 +0800

    changed to JpaRepository

--- src/main/java/sneakerstore/repositories/SneakerRepository.java
+++ src/main/java/sneakerstore/repositories/SneakerRepository.java
@@ -1,12 +1,13 @@
 package sneakerstore.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import sneakerstore.entities.Sneaker;
+        import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.repository.CrudRepository;
+        import org.springframework.stereotype.Repository;
+        import sneakerstore.entities.Sneaker;
 
-import java.util.List;
+        import java.util.List;
 
 @Repository
-public interface SneakerRepository extends CrudRepository<Sneaker, Long> {
+public interface SneakerRepository extends JpaRepository<Sneaker, Long> {
     List<Sneaker> findByModel(String model);
 }
